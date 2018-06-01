72ff269f2c6765aac7b602d06a890aaec00835f2
Jordy Manner <jordy.manner@milkcreation.fr>
2018-06-01 17:26:01 +0000

    outdated-browser -- trunk -- migration tiFy v2 phase #9

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+/**
+ * OutdatedBrowser
+ * @see https://www.npmjs.com/package/outdated-browser
+ * @internal npm i outdated-browser
+ */
+import 'outdated-browser/outdatedbrowser/outdatedbrowser.scss';
+import 'outdated-browser/outdatedbrowser/outdatedbrowser';
+
+import './css/styles.css';
+import './js/scripts';
