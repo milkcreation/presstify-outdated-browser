147ddfca86e163b2b5855c7bea361519f613f66b
Jordy Manner <jordy.manner@milkcreation.fr>
2018-08-29 07:56:34 +0000

    outdated-browser -- tag 1.4.3 -- Nouvelle version compatible v1.4 + v2.0

--- OutdatedBrowser.php
+++ OutdatedBrowser.php
@@ -2,55 +2,82 @@
 
 /**
  * @name Outdated Browser
- * @desc Extension PresstiFy de contrôle et de mise à jour de navigateur internet obsolète..
+ * @desc Extension PresstiFy de contrôle et de mise à jour de navigateur internet obsolète.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstiFy
  * @namespace \tiFy\Plugins\AdminUi
- * @version 2.0.1
+ * @version 1.4.3
  */
 
 namespace tiFy\Plugins\OutdatedBrowser;
 
-use tiFy\Apps\AppController;
+use Illuminate\Support\Arr;
+use tiFy\App\Dependency\AbstractAppDependency;
 
 /**
+ * Class OutdatedBrowser
+ * @package tiFy\Plugins\OutdatedBrowser
  * @see http://outdatedbrowser.com/fr
  * @see https://github.com/burocratik/Outdated-Browser/tree/master
  *
- * Lower Than (<):
- * "IE11","borderImage"
- * "IE10", "transform" (Default property)
- * "IE9", "boxShadow"
- * "IE8", "borderSpacing"
+ * Activation :
+ * ----------------------------------------------------------------------------------------------------
+ * Dans config/app.php ajouter \tiFy\Plugins\OutdatedBrowser\OutdatedBrowser à la liste des fournisseurs de services chargés automatiquement par l'application.
+ * ex.
+ * <?php
+ * ...
+ * use tiFy\Plugins\OutdatedBrowser\OutdatedBrowser;
+ * ...
+ *
+ * return [
+ *      ...
+ *      'providers' => [
+ *          ...
+ *          OutdatedBrowser::class
+ *          ...
+ *      ]
+ * ];
+ *
+ * Configuration :
+ * ----------------------------------------------------------------------------------------------------
+ * Dans le dossier de config, créer le fichier social.php
+ * @see /vendor/presstify-plugins/social/Resources/config/social.php Exemple de configuration
  */
-final class OutdatedBrowser extends AppController
+final class OutdatedBrowser extends AbstractAppDependency
 {
     /**
      * Liste des attributs de configuration.
      * @var array
      */
     protected $attributes = [
-        'bgColor'      => '#F25648',
-        'color'        => '#FFF',
-        'lowerThan'    => 'transform',
-        'languagePath' => '',
-        'wp_enqueue'   => true,
+        'bgColor'               => '#F25648',
+        'color'                 => '#FFF',
+        'lowerThan'             => 'transform',
+        'languagePath'          => '',
+        'wp_enqueue_scripts'    => true
     ];
 
     /**
-     * Initialisation du controleur.
-     *
-     * @return void
+     * {@inheritdoc}
      */
-    public function appBoot()
+    public function boot()
     {
-        $this->appTemplates(
-            ['directory' => $this->appDirname() . '/templates']
-        );
+        $this->app->appAddAction('init', [$this, 'init']);
+        $this->app->appAddAction('wp_enqueue_scripts', [$this, 'wp_enqueue_scripts']);
+        $this->app->appAddAction('wp_footer', [$this, 'wp_footer'], 99);
+    }
 
-        $this->appAddAction('init');
-        $this->appAddAction('wp_enqueue_scripts');
-        $this->appAddAction('wp_footer', null, 99);
+    /**
+     * Récupération d'un attribut de configuration.
+     *
+     * @param string $key Clé d'indice de l'attribut. Syntaxe à point permise.
+     * @param mixed $defaults Valeur de retour par défaut.
+     *
+     * @return mixed
+     */
+    public function get($key, $defaults = null)
+    {
+        return Arr::get($this->attributes, $key, $defaults);
     }
 
     /**
@@ -60,47 +87,39 @@ final class OutdatedBrowser extends AppController
      */
     public function init()
     {
-        $this->appSet(
-            'config',
-            array_merge(
-                $this->attributes,
-                $this->appConfig()
-            )
+        $this->attributes = array_merge(
+            $this->attributes,
+            config('outdated-browser')
         );
-        $this->appAssets()->setDataJs(
+
+        $this->app->appAssets()->setDataJs(
             'outdatedBrowser',
             [
-                'bgColor'      => $this->appConfig('bgColor'),
-                'color'        => $this->appConfig('color'),
-                'lowerThan'    => $this->appConfig('lowerThan'),
-                'languagePath' => $this->appConfig('languagePath'),
+                'bgColor'      => $this->get('bgColor'),
+                'color'        => $this->get('color'),
+                'lowerThan'    => $this->get('lowerThan'),
+                'languagePath' => $this->get('languagePath'),
             ]
         );
 
         \wp_register_style(
             'outdatedBrowser',
-            '//cdn.rawgit.com/burocratik/outdated-browser/develop/outdatedbrowser/outdatedbrowser.min.css',
+            class_info($this)->getUrl() . '/Resources/assets/css/outdatedbrowser.min.css',
             [],
             '1.1.2'
         );
         \wp_register_style(
-            'tiFyPluginOutdatedBrowser',
-            $this->appUrl() . '/assets/css/styles.css',
+            'tiFyOutdatedBrowser',
+            class_info($this)->getUrl() . '/Resources/assets/css/styles.css',
             ['outdatedBrowser'],
-            290518
+            180829
         );
+
         \wp_register_script(
-            'outdatedBrowser',
-            '//cdn.rawgit.com/burocratik/outdated-browser/develop/outdatedbrowser/outdatedbrowser.min.js',
+            'tiFyOutdatedBrowser',
+            class_info($this)->getUrl() . '/Resources/assets/js/scripts.min.js',
             [],
-            '1.1.2',
-            true
-        );
-        \wp_register_script(
-            'tiFyPluginOutdatedBrowser',
-            $this->appUrl() . '/assets/js/scripts.js',
-            ['outdatedBrowser'],
-            290518,
+            180829,
             true
         );
     }
@@ -112,9 +131,9 @@ final class OutdatedBrowser extends AppController
      */
     public function wp_enqueue_scripts()
     {
-        if ($this->appConfig('wp_enqueue')) :
-            \wp_enqueue_style('tiFyPluginOutdatedBrowser');
-            \wp_enqueue_script('tiFyPluginOutdatedBrowser');
+        if ($this->get('wp_enqueue_scripts')) :
+            \wp_enqueue_style('tiFyOutdatedBrowser');
+            \wp_enqueue_script('tiFyOutdatedBrowser');
         endif;
     }
 
@@ -125,8 +144,10 @@ final class OutdatedBrowser extends AppController
      */
     public function wp_footer()
     {
-        if (!$this->appConfig('languagePath')) :
-            echo $this->appTemplateRender('outdated-browser');
+        if (!$this->get('languagePath')) :
+            echo view()
+                ->setDirectory(__DIR__ . '/Resources/views')
+                ->render('outdated-browser');
         endif;
     }
 }
