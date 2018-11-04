c04e0ab759b34a6dacf90c53c42d6d5af00a6ed9
Jordy Manner <jordy.manner@milkcreation.fr>
2018-11-04 17:52:13 +0000

    outdated-browser -- tag 2.0.7 -- Correctif view Controller

--- Resources/views/outdated-browser.php
+++ Resources/views/outdated-browser.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @var \tiFy\Apps\Templates\TemplateBaseController $this
+ * @var tiFy\View\ViewController $this
  */
 ?>
 
@@ -8,11 +8,12 @@
     <h6><?php _e('La version de votre navigateur est trop ancienne', 'tify'); ?></h6>
 
     <p>
-        <?php _e('Pour afficher de manière satisfaisante le contenu de ce site', 'tify'); ?>
+        <?php _e('Vous ne pourrez pas afficher de manière optimale le contenu de ce site.', 'tify'); ?>
         <a id="btnUpdateBrowser" href="http://outdatedbrowser.com/fr" target="_blank">
-            <?php _e('Télécharger Google Chrome', 'tify'); ?>
+            <?php _e('Télécharger', 'tify'); ?>
         </a>
     </p>
+
     <p class="last">
         <a href="#" id="btnCloseUpdateBrowser" title="<?php _e('Fermer', 'tify'); ?>">&times;</a>
     </p>
