3e4ca34ffe15b1e39ab690966b9dd27de117d0c9
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-12-11 13:54:24 +0000

    outdated-browser -- tag 1.4.27 -- Version bridgée

---
+++ outdated-browser/tags/1.4.12/Resources/views/outdated-browser.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @var tiFy\View\ViewController $this
+ */
+?>
+<div id="outdated" class="tiFyOutdatedBrowser">
+    <h6><?php _e('La version de votre navigateur est trop ancienne', 'tify'); ?></h6>
+
+    <p>
+        <?php _e('Vous ne pourrez pas afficher de manière optimale le contenu de ce site.', 'tify'); ?>
+        <a id="btnUpdateBrowser" href="http://outdatedbrowser.com/fr" target="_blank">
+            <?php _e('Télécharger', 'tify'); ?>
+        </a>
+    </p>
+
+    <p class="last">
+        <a href="#" id="btnCloseUpdateBrowser" title="<?php _e('Fermer', 'tify'); ?>">&times;</a>
+    </p>
+</div>
