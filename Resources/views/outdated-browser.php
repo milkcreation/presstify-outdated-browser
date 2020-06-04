f3f45ddfe044e9197789bac89739a048e2232fc9
Jordy Manner <jordy.manner@milkcreation.fr>
2020-06-04 06:52:41 +0000

    outdated-browser -- 2.0.18 -- lien ext norel

---
+++ Resources/views/outdated-browser.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @var tiFy\Contracts\View\PlatesFactory $this
+ */
+?>
+<div id="outdated" class="tiFyOutdatedBrowser">
+    <div class="OutdatedBrowser-title"><?php _e('La version de votre navigateur est trop ancienne', 'tify'); ?></div>
+
+    <p class="OutdatedBrowser-text">
+        <?php _e('Vous ne pourrez pas afficher de manière optimale le contenu de ce site.', 'tify'); ?>
+        <a class="OutdatedBrowser-upload" href="http://outdatedbrowser.com/fr" target="_blank" rel="noreferrer">
+            <?php _e('Télécharger', 'tify'); ?>
+        </a>
+    </p>
+
+    <a href="#" class="OutdatedBrowser-close" title="<?php _e('Fermer', 'tify'); ?>">&times;</a>
+</div>
